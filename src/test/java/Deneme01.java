fab807025e68e13301c5cdaf2dcd7656e30a3a4a
Sefa Eyer <eyersefa@gmail.com>
2023-02-25 19:28:19 +0300

    v4

--- src/test/java/Deneme01.java
+++ src/test/java/Deneme01.java
@@ -2,6 +2,7 @@ public class Deneme01 {
     public static void main(String[] args) {
         System.out.println("Merhaba");
         System.out.println("Dunya");
+        System.out.println("B129");
     }
 }
 //restore --> tamamen workin space i temizler
